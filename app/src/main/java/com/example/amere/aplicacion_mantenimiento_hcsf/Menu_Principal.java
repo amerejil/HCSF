4c9f41163d30fd725e28de7a0a823c4796c2e3c7
Alexander_Merejildo <amerejil@outlook.com>
2018-11-23 23:08:57 -0500

    comenzando_a_programar_

---
+++ app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Menu_Principal.java
@@ -0,0 +1,13 @@
+package com.example.amere.aplicacion_mantenimiento_hcsf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__principal);
+    }
+}
