19178e97bc80c8d0e645789e51db08a276df2414
amere <amerejil@outlook.com>
2018-12-03 15:05:22 -0500

    añadiendo activities trabajos mensuales y trabajos finalizados diarios y mensuales

--- app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Menu_Principal.java
+++ app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Menu_Principal.java
@@ -1,13 +1,51 @@
 package com.example.amere.aplicacion_mantenimiento_hcsf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu_Principal extends AppCompatActivity {
-
+    private Button button_trabajos_diarios;
+    private Button button_trabajos_mensuales;
+    private Button button_trabajos_diarios_finalizados;
+    private Button button_trabajos_mensuales_finalizados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu__principal);
+        button_trabajos_diarios=(Button)findViewById(R.id.buttonDailyWork);
+        button_trabajos_mensuales=(Button)findViewById(R.id.buttonMonthlyWork);
+        button_trabajos_diarios_finalizados=(Button)findViewById(R.id.buttonDailyWorkFinished);
+        button_trabajos_mensuales_finalizados=(Button)findViewById(R.id.buttonMonthlyWorkFinished);
+        button_trabajos_diarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Menu_Principal.this,Trabajos_Diarios.class);
+                startActivity(intent);
+            }
+        });
+        button_trabajos_mensuales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Menu_Principal.this,Trabajos_Mensuales.class);
+                startActivity(intent);
+            }
+        });
+        button_trabajos_diarios_finalizados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Menu_Principal.this,Trabajos_Diarios_Finalizados.class);
+                startActivity(intent);
+            }
+        });
+        button_trabajos_mensuales_finalizados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Menu_Principal.this,Trabajos_Mensuales_Finalizados.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Trabajos_Mensuales_Finalizados.java
+++ app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Trabajos_Mensuales_Finalizados.java
@@ -3,12 +3,12 @@ package com.example.amere.aplicacion_mantenimiento_hcsf;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Trabajos_Diarios extends AppCompatActivity {
+public class Trabajos_Mensuales_Finalizados extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trabajos__diarios);
+        setContentView(R.layout.activity_trabajos_mensuales_finalizados);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
