c318aee94aae01779f46677d9d7914f02c43736b
Mario Andres Barzola <mario0barzola@gmail.com>
2018-12-27 07:20:16 -0500

    se modifico el codigo Trabajos_Mensuales.java copiando funciones de Agregar_trabajos_diarios.java, revisar comentarios

--- app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Activities/Trabajos_Mensuales.java
+++ app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Activities/Trabajos_Mensuales.java
@@ -11,8 +11,13 @@ package com.example.amere.aplicacion_mantenimiento_hcsf.Activities;
         import android.support.v7.widget.LinearLayoutManager;
         import android.support.v7.widget.RecyclerView;
         import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.EditText;
         import android.widget.RelativeLayout;
+        import android.widget.Spinner;
         import android.widget.TextView;
+        import android.widget.Toast;
 
         import com.example.amere.aplicacion_mantenimiento_hcsf.Adapters.Adapter_for_task_list;
         import com.example.amere.aplicacion_mantenimiento_hcsf.OnSwipeTouchListener;
@@ -27,6 +32,7 @@ package com.example.amere.aplicacion_mantenimiento_hcsf.Activities;
         import com.google.firebase.database.Query;
         import com.google.firebase.database.ValueEventListener;
 
+        import java.text.SimpleDateFormat;
         import java.util.ArrayList;
 
 public class Trabajos_Mensuales extends AppCompatActivity {
@@ -46,6 +52,33 @@ public class Trabajos_Mensuales extends AppCompatActivity {
     private int click_text_date;
     private int click_text_state;
     private SharedPreferences preferences;
+
+    ///agregue variables de agregar trabajos diarios
+    //ojo si se comentan es porque arriba ya existen
+    /*************************************************************************/
+    private Spinner tipo;
+    private String stringTipo;
+    private String stringUbicacion;
+    private String stringPiso;
+    private String stringAtencion;
+    private String stringArea;
+    private String stringSolicitante;
+    private String stringSubarea;
+    private String stringTrabajoSolicitado;
+    private String stringFecha_inicio_entero;
+    private EditText area;
+    private EditText subarea;
+    private EditText solicitante;
+    private EditText trabajo_solicitado;
+    private Spinner ubicacion;
+    private Spinner piso;
+    private Spinner atencion;
+    private FloatingActionButton enviarTarea;
+   // private DatabaseReference task;
+   // private FirebaseDatabase database_hcsf;
+    private String stringFecha_inicio;
+    private String stringEstado;
+/*************************************************************************/
 
     @SuppressLint("ClickableViewAccessibility")
 
@@ -77,6 +110,36 @@ public class Trabajos_Mensuales extends AppCompatActivity {
         final Query orden_fecha=task.orderByChild("fecha_inicio_entero");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //se agrego funcionalidades y declaraciones provenientes de agregar trabajos diarios
+        //ojo si se comentan es porque arriba ya existen o se reemplazan por el del layout correspondiente
+/*************************************************************************/
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+       // super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //setContentView(R.layout.activity_agregar_trabajos_diarios);  //se comento para reemplazarlo y definir abajo
+        setContentView(R.layout.activity_trabajos__mensuales); //este layout por el de trabajos mensuales
+        area=findViewById(R.id.editTextArea);
+        subarea=findViewById(R.id.editTextSubrea);
+        solicitante=findViewById(R.id.editTextSolicitante);
+        trabajo_solicitado=findViewById(R.id.editText);
+        tipo=findViewById(R.id.spinner1);
+        ubicacion=findViewById(R.id.spinner2);
+        piso=findViewById(R.id.spinner3);
+        atencion=findViewById(R.id.spinner4);
+        enviarTarea=findViewById(R.id.floatingActionButton1);
+        database_hcsf= FirebaseDatabase.getInstance();
+        //task=database_hcsf.getReference("Tareas");                //se comento el path de la base tareas porveniente de agregar trabajos diarios
+        task=database_hcsf.getReference("Tareas_Mensuales"); //por uno llamado Tareas _Mensuales
+        ArrayAdapter<CharSequence> adapterTipo=ArrayAdapter.createFromResource(this,R.array.tipo,R.layout.spinner_item);
+        ArrayAdapter<CharSequence> adapterUbicacion=ArrayAdapter.createFromResource(this,R.array.ubicacion,R.layout.spinner_item);
+        ArrayAdapter<CharSequence> adapterPiso=ArrayAdapter.createFromResource(this,R.array.piso,R.layout.spinner_item);
+        ArrayAdapter<CharSequence> adapterAtemcion=ArrayAdapter.createFromResource(this,R.array.atencion,R.layout.spinner_item);
+        ubicacion.setAdapter(adapterUbicacion);
+        tipo.setAdapter(adapterTipo);
+        piso.setAdapter(adapterPiso);
+        atencion.setAdapter(adapterAtemcion);
+
+/*************************************************************************/
         textViewType.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -307,7 +370,6 @@ public class Trabajos_Mensuales extends AppCompatActivity {
             }
         });
 
-
         floatingActionButtonAddDailyTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -319,6 +381,77 @@ public class Trabajos_Mensuales extends AppCompatActivity {
 
             }
         });
+        //Se agrego la implementacion de las funcionalidades provenientes de Agregar_Trabajos_diarios
+        //********************************************************************************/
+        tipo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                stringTipo= parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        ubicacion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                stringUbicacion=parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        piso.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                stringPiso=parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        atencion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                stringAtencion=parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        enviarTarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long date=System.currentTimeMillis();
+                stringEstado="No iniciado";
+                SimpleDateFormat f_date=new SimpleDateFormat("dd/MM/yyyy");
+                SimpleDateFormat f_date_entero=new SimpleDateFormat("ddMMyyyy");
+                stringFecha_inicio_entero =f_date_entero.format(date);
+                stringFecha_inicio=f_date.format(date);
+                stringSubarea=subarea.getText().toString();
+                stringSolicitante=solicitante.getText().toString();
+                stringTrabajoSolicitado=trabajo_solicitado.getText().toString();
+                stringArea=area.getText().toString();
+                String value=task.push().getKey();
+                Toast.makeText(Trabajos_Mensuales.this,stringFecha_inicio,Toast.LENGTH_SHORT).show();
+                task.child(value).setValue(new data_task(value,stringTipo,stringUbicacion,stringPiso,stringArea,
+                        stringSubarea,stringAtencion,stringSolicitante,stringTrabajoSolicitado,stringFecha_inicio, stringFecha_inicio_entero,"","",stringEstado,"Desconocido","") );
+            }
+        });
+
+
+
+        //*********************************************************************************/
 
     }
 
