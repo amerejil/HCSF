3954a0279ecc89bf9f65e1379d62806111cd0e72
Alexander_Merejildo <amerejil@outlook.com>
2019-02-06 12:10:35 -0500

    ultimas modificaciones a los layout

--- app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Activities/Menu_Principal.java
+++ app/src/main/java/com/example/amere/aplicacion_mantenimiento_hcsf/Activities/Menu_Principal.java
@@ -1,18 +1,27 @@
 package com.example.amere.aplicacion_mantenimiento_hcsf.Activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.amere.aplicacion_mantenimiento_hcsf.Adapters.Adaptader_for_principal_menu;
@@ -33,7 +42,6 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -43,7 +51,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicMarkableReference;
+
 
 public class Menu_Principal extends AppCompatActivity {
 
@@ -109,116 +117,6 @@ public class Menu_Principal extends AppCompatActivity {
                 Toast.makeText(Menu_Principal.this, "adm", Toast.LENGTH_SHORT).show();
             }
         }
-        final Workbook wb = new HSSFWorkbook();
-        data=new data_task();
-          final ValueEventListener listener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-
-                Sheet sheet1 = wb.createSheet("Tareas Diarias");
-                Row row=sheet1.createRow(0);
-                row.createCell(0).setCellValue("Fecha");
-                row.createCell(1).setCellValue("Tipo");
-                row.createCell(2).setCellValue("Subtipo");
-                row.createCell(6).setCellValue("Ubicación");
-                row.createCell(3).setCellValue("Piso");
-                row.createCell(4).setCellValue("Área");
-                row.createCell(5).setCellValue("Subárea");
-
-                for (DataSnapshot datasnapshot : dataSnapshot.getChildren()) {
-                    data=datasnapshot.getValue(data_task.class);
-                    row=sheet1.createRow(cont);
-                    row.createCell(0).setCellValue(data.getFecha_inicio());
-                    row.createCell(1).setCellValue(data.getTipo());
-                    row.createCell(2).setCellValue(data.getSubtipo());
-                    row.createCell(6).setCellValue(data.getUbicacion());
-                    row.createCell(3).setCellValue(data.getPiso());
-                    row.createCell(4).setCellValue(data.getArea());
-                    row.createCell(5).setCellValue(data.getSubarea());
-
-                    cont++;
-
-
-
-                }
-                cont=1;
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-        final ValueEventListener listener_m = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-
-                Sheet sheet2 = wb.createSheet("Tareas Mensuales");
-                Row row=sheet2.createRow(0);
-                row.createCell(0).setCellValue("Fecha");
-                row.createCell(1).setCellValue("Tipo");
-                row.createCell(2).setCellValue("Subtipo");
-                row.createCell(3).setCellValue("Piso");
-                row.createCell(4).setCellValue("Área");
-                row.createCell(5).setCellValue("Subárea");
-                row.createCell(6).setCellValue("Ubicación");
-
-                for (DataSnapshot datasnapshot : dataSnapshot.getChildren()) {
-                    data=datasnapshot.getValue(data_task.class);
-                    row=sheet2.createRow(cont);
-                    row.createCell(0).setCellValue(data.getFecha_inicio());
-                    row.createCell(1).setCellValue(data.getTipo());
-                    row.createCell(2).setCellValue(data.getSubtipo());
-                    row.createCell(3).setCellValue(data.getPiso());
-                    row.createCell(4).setCellValue(data.getArea());
-                    row.createCell(5).setCellValue(data.getSubarea());
-                    row.createCell(6).setCellValue(data.getUbicacion());
-                    cont++;
-
-
-                }
-                OutputStream fileOut = null ;
-                try {
-                    fileOut = openFileOutput("Reporte de Tareas HCSF.xls",Context.MODE_PRIVATE);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    wb.write(fileOut);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    fileOut.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-            diarioTask.addListenerForSingleValueEvent(listener);
-        mensualTask.addListenerForSingleValueEvent(listener_m);
-        Uri file = Uri.fromFile(new File(getFilesDir()+"/Reporte de Tareas HCSF.xls"));
-        StorageReference storageRef = storage.getReference("tareas/"+file.getLastPathSegment());
-
-        UploadTask uploadTask =storageRef.putFile(file);
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(Menu_Principal.this, "Fail", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(Menu_Principal.this, "Successfully", Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
         final ValueEventListener listener1 = new ValueEventListener() {
@@ -232,7 +130,6 @@ public class Menu_Principal extends AppCompatActivity {
 
             }
         };
-
 
         diarioTask.addValueEventListener(listener1);
         mensualTask.addValueEventListener(listener1);
@@ -270,6 +167,17 @@ public class Menu_Principal extends AppCompatActivity {
 
         recyclerView_menu_principal.setAdapter(adaptador);
     }
+    public static class PermissionCheck {
+        public static boolean readAndWriteExternalStorage(Context context) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            } else {
+                return true;
+            }
+
+        }
+    }
     /*public int ordenar_fecha(int fecha_desorden)
     {
         int temp1=fecha_desorden%10000;
@@ -286,5 +194,189 @@ public class Menu_Principal extends AppCompatActivity {
         int temp3=Integer.parseInt(fecha_finalizacion.substring(3,5));
         return temp1*10000+temp3*100+temp2;
     }*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        String tipo = preferences.getString("administrador", "usuario");
+        if (tipo != null) {
+            if (tipo.equals("usuario")) {
+
+                return false;
+            }
+            else
+            {
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.download_menu, menu);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.download_menu: {
+                final Workbook wb = new HSSFWorkbook();
+                data=new data_task();
+                final ValueEventListener listener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                        Sheet sheet1 = wb.createSheet("Tareas Diarias");
+                        Row row=sheet1.createRow(0);
+                        row.createCell(0).setCellValue("Fecha de inicio");
+                        row.createCell(1).setCellValue("Fecha de Finalización");
+                        row.createCell(2).setCellValue("Tipo");
+                        row.createCell(3).setCellValue("Subtipo");
+                        row.createCell(4).setCellValue("Ubicación");
+                        row.createCell(5).setCellValue("Piso");
+                        row.createCell(6).setCellValue("Área");
+                        row.createCell(7).setCellValue("Subárea");
+                        row.createCell(8).setCellValue("Trabajo solicitado");
+                        row.createCell(9).setCellValue("Nota");
+                        row.createCell(10).setCellValue("Estado");
+
+                        for (DataSnapshot datasnapshot : dataSnapshot.getChildren()) {
+                            data=datasnapshot.getValue(data_task.class);
+                            row=sheet1.createRow(cont);
+                            row.createCell(0).setCellValue(data.getFecha_inicio());
+                            row.createCell(1).setCellValue(data.getFecha_finalizacion());
+                            row.createCell(2).setCellValue(data.getTipo());
+                            row.createCell(3).setCellValue(data.getSubtipo());
+                            row.createCell(4).setCellValue(data.getUbicacion());
+                            row.createCell(5).setCellValue(data.getPiso());
+                            row.createCell(6).setCellValue(data.getArea());
+                            row.createCell(7).setCellValue(data.getSubarea());
+                            row.createCell(8).setCellValue(data.getTrabajo_solicitado());
+                            row.createCell(9).setCellValue(data.getNota());
+                            row.createCell(10).setCellValue(data.getEstado());
+
+                            cont++;
+
+
+
+                        }
+                        cont=1;
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                };
+                final ValueEventListener listener_m = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                        Sheet sheet2 = wb.createSheet("Tareas Mensuales");
+                        Row row=sheet2.createRow(0);
+                        row.createCell(0).setCellValue("Fecha de inicio");
+                        row.createCell(1).setCellValue("Fecha de Finalización");
+                        row.createCell(2).setCellValue("Tipo");
+                        row.createCell(3).setCellValue("Subtipo");
+                        row.createCell(4).setCellValue("Ubicación");
+                        row.createCell(5).setCellValue("Piso");
+                        row.createCell(6).setCellValue("Área");
+                        row.createCell(7).setCellValue("Subárea");
+                        row.createCell(8).setCellValue("Trabajo solicitado");
+                        row.createCell(9).setCellValue("Nota");
+                        row.createCell(10).setCellValue("Estado");
+                        for (DataSnapshot datasnapshot : dataSnapshot.getChildren()) {
+                            data=datasnapshot.getValue(data_task.class);
+                            row=sheet2.createRow(cont);
+                            row.createCell(0).setCellValue(data.getFecha_inicio());
+                            row.createCell(1).setCellValue(data.getFecha_finalizacion());
+                            row.createCell(2).setCellValue(data.getTipo());
+                            row.createCell(3).setCellValue(data.getSubtipo());
+                            row.createCell(4).setCellValue(data.getUbicacion());
+                            row.createCell(5).setCellValue(data.getPiso());
+                            row.createCell(6).setCellValue(data.getArea());
+                            row.createCell(7).setCellValue(data.getSubarea());
+                            row.createCell(8).setCellValue(data.getTrabajo_solicitado());
+                            row.createCell(9).setCellValue(data.getNota());
+                            row.createCell(10).setCellValue(data.getEstado());
+
+                            cont++;
+
+
+                        }
+                        OutputStream fileOut = null ;
+                        try {
+                            fileOut = openFileOutput("Reporte de Tareas HCSF.xls",Context.MODE_PRIVATE);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            wb.write(fileOut);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            fileOut.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                };
+                diarioTask.addListenerForSingleValueEvent(listener);
+                mensualTask.addListenerForSingleValueEvent(listener_m);
+                Uri file = Uri.fromFile(new File(getFilesDir()+"/Reporte de Tareas HCSF.xls"));
+                StorageReference storageRef = storage.getReference("tareas/"+file.getLastPathSegment());
+
+                UploadTask uploadTask =storageRef.putFile(file);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Menu_Principal.this, "Fail", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //Toast.makeText(Menu_Principal.this, "Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        if(PermissionCheck.readAndWriteExternalStorage(Menu_Principal.this)){
+                            DownloadManager.Request request = new DownloadManager.Request(uri);
+                            request.setDescription("Reporte Mantenimiento");
+                            request.setTitle("Reporte");
+                            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Reporte de Tareas HCSF.xls");
+
+                            // get download service and enqueue file
+                            DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                            manager.enqueue(request);
+                            Toast.makeText(Menu_Principal.this, "Iniciando descarga", Toast.LENGTH_SHORT).show();
+                            //Your read write code.
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+
+                return true;
+            }
+            default:
+                return false;
+        }
+    }
 }
 
